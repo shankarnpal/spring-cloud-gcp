67513ba0a3478d9f857593f497f4245547c76f52
ChengyuanZhao <cccz@google.com>
2018-01-22 14:00:52 -0500

    brought over Ray's starter/autoconfig project

---
+++ spring-cloud-gcp-starters/spring-data-spanner-starter/src/main/java/org/springframework/data/spanner/autoconfiguration/SpannerProperties.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2017 original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.springframework.data.spanner.autoconfiguration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by rayt on 3/20/17.
+ */
+@ConfigurationProperties("gcp.spanner")
+public class SpannerProperties {
+	private String projectId;
+	private String instanceId;
+	private String database;
+
+	public String getProjectId() {
+		return this.projectId;
+	}
+
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getInstanceId() {
+		return this.instanceId;
+	}
+
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+	}
+
+	public String getDatabase() {
+		return this.database;
+	}
+
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+}
